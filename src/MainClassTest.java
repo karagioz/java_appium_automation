f6f9f7873aa41abf19efba85a304eb678f695594
Daria Kuznetsova <karagioz@yandex.ru>
2019-12-28 21:36:51 +0200

    Test 2: testGetClassNumber

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -1,12 +1,27 @@
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class MainClassTest {
+    private static MainClass mainClass;
+
+    @BeforeClass
+    public static void setUp() {
+        mainClass = new MainClass();
+    }
+
     @Test
     public void testGetLocalNumber() {
-        MainClass mainClass = new MainClass();
         int expected = 14;
         int actual = mainClass.getLocalNumber();
         Assert.assertEquals("Method getLocalNumber returns unexpected value", expected, actual);
+    }
+
+    @Test
+    public void testGetClassNumber() {
+        int threshold = 45;
+        int actual = mainClass.getClassNumber();
+        Assert.assertTrue("Method getClassNumber returns value less than threshold " + threshold,
+                actual > threshold);
     }
 }
