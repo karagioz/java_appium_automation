ca5875a58fe8d28dc38a9238b50f0632b46bb2a9
Daria Kuznetsova <karagioz@yandex.ru>
2019-12-28 20:59:16 +0200

    Basics of JUnit

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,13 +1,25 @@
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 public class MainTest extends CoreTestCase {
-    @Test
-    public void myFirstTest() {
+    @Before
+    public void textStartTest(){
+        System.out.println("Start test");
+    }
 
+    @After
+    public void textFinishTest(){
+        System.out.println("Finish test");
     }
 
     @Test
-    public void mySecondTest() {
+    public void firstTest() {
+        System.out.println("First test");
+    }
 
+    @Test
+    public void secondTest() {
+        System.out.println("Second test");
     }
 }
